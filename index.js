125c77b4596b0e9eaf24e421cd6f2ac265f28a7c
Evgeniy Kalimullin <counsil1986@gmail.com>
2021-09-30 22:36:15 +0700

    added job and parser dividends

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const express = require('express')
+const cron = require('node-cron')
+var puppeteer = require('puppeteer')
 require('dotenv').config()
-const sequelize = require('./db')
-const models = require('./models/index')
+// const sequelize = require('./db')
+// const models = require('./models/index')
 // const issuerRouter = require('./routes/issuer.routes')
 // const tradingRouter = require('./routes/trading.routes')
 
@@ -15,9 +17,35 @@ app.use(express.json())
 
 const start = async () => {
   try {
-    await sequelize.authenticate()
-    await sequelize.sync()
+    // await sequelize.authenticate()
+    // await sequelize.sync()
     app.listen(PORT, () => console.log(`Server has been started on port: ${PORT}`))
+
+    cron.schedule('* * * * *', async () => {
+      const browser = await puppeteer.launch()
+      const page = await browser.newPage()
+      await page.goto('https://www.dohod.ru/ik/analytics/dividend/sberp')
+      await page.waitForSelector('.table-title')
+      const amountDividends = await page.evaluate(() =>
+        Array.from(
+          document.querySelectorAll('#leftside-col > table:nth-child(9) > tbody > tr:not(.forecast) > td:nth-child(4)'),
+          (el) => el.innerHTML
+        )
+      )
+      const dates = await page.evaluate(() =>
+        Array.from(
+          document.querySelectorAll('#leftside-col > table:nth-child(9) > tbody > tr:not(.forecast) > td:nth-child(2)'),
+          (el) => el.innerHTML
+        )
+      )
+
+      const dividends = amountDividends.map((item, index) => ({
+        date: dates[index],
+        amount: item,
+      }))
+      console.log('>>', dividends)
+      await browser.close()
+    })
   } catch (error) {
     console.log(error)
   }
